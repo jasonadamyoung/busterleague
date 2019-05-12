3927a606f27e4d7b850677441b980539ce7c25c9
Jason Young <jay@outfielding.net>
2019-05-12 17:11:59 -0400

    fix names

--- app/models/roster.rb
+++ app/models/roster.rb
@@ -38,52 +38,59 @@ class Roster < ApplicationRecord
     self.update_attribute(:player_id, player.id)
   end
 
-  def self.match_team_season_names(team_id,season,names_hash,is_pitching)
-    match_data = {}
-    names_hash.each do |name,position|
-      namefinder = self.idiotic_shorthand_name_translations(name.dup)
-      (lastname,startswith) = namefinder.split(',')
-      if(lastname.last =~ %r{[A-Z]})
-        startswith = lastname.last
-        lastname.chop!
-      end 
-      nameparts = lastname.split("'")
-      finder = nameparts.max_by(&:length)
-      end_name = finder.downcase.split(' ').last
-      player_finder = self.where(season: season).where(team_id: team_id).where("end_name ILIKE '%#{end_name}%'")
-      if(is_pitching)
-        player_finder = player_finder.pitchers
-      elsif(position == 'p')
-        player_finder = player_finder.pitchers
-      end
-      if(found_players = player_finder.all)
-        if(found_players.size == 1)
-          match_data[name] = found_players[0].id
-        elsif(found_players.size > 1)
-          # found more than one
-          found_player_id = -1
-          found_players.each do |roster_player|
-            matcher = Regexp.new("^#{startswith}")
-            if(roster_player.name =~ matcher)
-              found_player_id = roster_player.id
-            end
+  def self.find_roster_for_name_position_team_season(name,position,team_id,season)
+    namefinder = self.idiotic_shorthand_name_translations(name.dup)
+    (lastname,startswith) = namefinder.split(',')
+    if(lastname.last =~ %r{[A-Z]})
+      startswith = lastname.last
+      lastname.chop!
+    end 
+    nameparts = lastname.split("'")
+    finder = nameparts.max_by(&:length)
+    end_name = finder.downcase.split(' ').last
+    player_finder = self.where(season: season).where(team_id: team_id).where("end_name ILIKE '%#{end_name}%'")
+    if(position == 'p')
+      player_finder = player_finder.pitchers
+    end
+
+    if(found_players = player_finder.all)
+      if(found_players.size == 1)
+        return found_players[0].id
+      elsif(found_players.size > 1)
+        # found more than one
+        found_player_id = -1
+        found_players.each do |roster_player|
+          matcher = Regexp.new("^#{startswith}")
+          if(roster_player.name =~ matcher)
+            return roster_player.id
           end
-          match_data[name] = found_player_id
-        else
-          match_data[name] = 0
         end
       else
-        match_data[name] = 0
+        return 0
       end
+    else
+      return 0
+    end
+  end
+
+  def self.match_team_season_names(team_id,season,names_hash)
+    match_data = {}
+    names_hash.each do |name,position|
+      match_data[name] = self.find_roster_for_name_position_team_season(name,position,team_id,season)
     end
     match_data
   end
-
 
   def self.idiotic_shorthand_name_translations(name)
     case name
     when "H'lndswrth"
       "Hollandsworth"
+    when "Ichiro"
+      "Suzuki"
+    when "DelosSntos"
+      "Santos"
+    when "LoDuca"
+      "Duca"
     else
       name
     end
