da298ff868c3f3642d8580fd6e72fb5a8510463b
Jason Young <jay@outfielding.net>
2019-06-05 21:32:42 -0400

    png logo creator

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,3 +1,9 @@
+# === COPYRIGHT:
+# Copyright (c) Jason Adam Young
+# === LICENSE:
+# see LICENSE file
+require 'rmagick'
+
 class WelcomeController < ApplicationController
 
   def home
@@ -15,6 +21,28 @@ class WelcomeController < ApplicationController
 
   def awards
   end
+
+  def teamlogo
+    @filename = params[:filename]
+    @team = Team.where(id: params[:id]).first
+    (width,height) = [100,100]
+    img, data = Magick::Image.from_blob(@team.svglogo) {
+      self.format = 'SVG'
+      self.background_color = 'transparent'
+    }
+    thumb = img.resize(width, height)
+    thumb.format = 'png'
+    thumb.background_color = "Transparent"
+    respond_to do |format|
+      format.png  { 
+        send_data(thumb.to_blob,
+                   :type  => 'image/png',
+                   :filename => @filename,
+                   :disposition => 'inline') 
+      }
+    end
+  end
+
 
 
   def ss
