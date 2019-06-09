529b2e2619f396ed2d25a9eb9057b78d418f510a
Jason Young <jay@outfielding.net>
2019-06-09 13:39:56 -0400

    can’t really update when the site is in 503 mode

--- db/migrate/20190609150340_add_total_stats_flag.rb
+++ db/migrate/20190609150340_add_total_stats_flag.rb
@@ -5,14 +5,12 @@ class AddTotalStatsFlag < ActiveRecord::Migration[5.2]
     BattingStat.reset_column_information
     BattingStat.fix_total_flags
     execute "UPDATE batting_stats SET eligible_games = 162 WHERE season < 2018"
-    Team.update_batting_stats_for_season(2018)
 
     add_column(:pitching_stats, :eligible_games, :integer, default: 0, null: false)
     add_column(:pitching_stats, :is_total, :boolean, default: true, null: false)
     PitchingStat.reset_column_information
     PitchingStat.fix_total_flags
     execute "UPDATE pitching_stats SET eligible_games = 162 WHERE season < 2018"
-    Team.update_pitching_stats_for_season(2018)
 
   end
 
