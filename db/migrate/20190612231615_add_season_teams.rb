d322d02c7563dd03f12b5f3284952645b9928864
Jason Young <jay@outfielding.net>
2019-06-15 20:50:00 -0400

    fix migration

--- db/migrate/20190612231615_add_season_teams.rb
+++ db/migrate/20190612231615_add_season_teams.rb
@@ -1,5 +1,5 @@
 class AddSeasonTeams < ActiveRecord::Migration[5.2]
-  def up
+  def change
     create_table "team_seasons", force: :cascade do |t|
       t.integer "season", limit: 2, null: false
       t.integer "team_id", limit: 2, null: false
@@ -13,21 +13,5 @@ class AddSeasonTeams < ActiveRecord::Migration[5.2]
       t.boolean "ws_win", null: false, default: false 
       t.index ["season","team_id"], name: "ts_ndx", unique: true
     end
-
-    TeamSeason.reset_column_information
-    (1999..2018).to_a.each do |season|
-      Team.all.each do |t|
-        ts = TeamSeason.new(season: season, team_id: t.id)
-        ts.web_team_id = (season == 1999) ? t.web_team_id_nn : t.web_team_id
-        ts.abbrev = t.abbrev
-        ts.league = t.league
-        ts.division = t.division
-        ts.owner_id = t.owner_id
-      end
-    end
-  end
-
-  def down
-    drop_table("team_seasons")
   end
 end
