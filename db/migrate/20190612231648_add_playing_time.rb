d87c01cdea187fbe499091c9a35e1c17c02638d4
Jason Young <jay@outfielding.net>
2019-06-15 20:50:13 -0400

    playing time migration

---
+++ db/migrate/20190612231648_add_playing_time.rb
@@ -0,0 +1,29 @@
+class AddPlayingTime < ActiveRecord::Migration[5.2]
+  def change
+    create_table "pitcher_playing_times", force: :cascade do |t|
+      t.integer "season", limit: 2, null: false
+      t.integer "player_id", limit: 2, null: false
+      t.integer "total_games"
+      t.float   "actual_ip"
+      t.integer "qualifying_ip"
+      t.float   "ip"
+      t.boolean "qualified"
+      t.index ["season","player_id"], name: "ppt_ndx", unique: true
+    end
+
+    create_table "batter_playing_times", force: :cascade do |t|
+      t.integer "season", limit: 2, null: false
+      t.integer "player_id", limit: 2, null: false
+      t.integer "total_games"
+      t.integer "actual_ab"
+      t.integer "qualifying_ab"
+      t.float   "allowed_percentage"
+      t.integer "allowed_starts"
+      t.float   "played_percentage"
+      t.integer "gs"
+      t.integer "ab"
+      t.boolean "qualified"
+      t.index ["season","player_id"], name: "bpt_ndx", unique: true
+    end
+  end
+end
