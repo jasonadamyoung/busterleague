bf14cc9385f158a1f5dbad9c24cb9fee038d0f93
Jason Young <jay@outfielding.net>
2019-08-17 21:50:01 -0400

    read the smtp host from the environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,10 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.smtp_settings = {
+    address:              ENV['SMTP_RELAY_HOST']
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
