dd57f8676ed033092f7ad65ff00c4d3d592bb627
Jason Young <jay@outfielding.net>
2020-06-06 09:49:35 -0400

    bump to ruby 2.6.6

--- build/gemset_setup.sh
+++ build/gemset_setup.sh
@@ -2,5 +2,5 @@
 # uninstall all versions of bundler
 /usr/local/rvm/bin/rvm @global do gem update --system
 /usr/local/rvm/bin/rvm @global do gem update bundler --force
-/usr/local/rvm/bin/rvm alias create default 2.6.5@webapp --create
+/usr/local/rvm/bin/rvm alias create default 2.6.6@webapp --create
 gem install pry
