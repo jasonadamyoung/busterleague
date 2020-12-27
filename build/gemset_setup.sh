a4d778d8cf9842f8d5fff411b9d0db965366d709
Jason Young <jay@outfielding.net>
2020-12-27 12:56:30 -0500

    dubya-tee-eff

--- build/gemset_setup.sh
+++ build/gemset_setup.sh
@@ -2,5 +2,6 @@
 # uninstall all versions of bundler
 /usr/local/rvm/bin/rvm @global do gem update --system
 /usr/local/rvm/bin/rvm @global do gem update bundler --force
+/usr/local/rvm/bin/rvm @global do gem install gem-wrappers
 /usr/local/rvm/bin/rvm alias create default 2.6.6@webapp --create
 gem install pry
