586c94d683429c5d6c4d57438bcd5b54765f439e
Jason Young <jay@outfielding.net>
2020-12-27 14:01:05 -0500

    Something is breaking in latest rubygems with gem wrapper

--- build/gemset_setup.sh
+++ build/gemset_setup.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-# uninstall all versions of bundler
-gem install gem-wrappers
-/usr/local/rvm/bin/rvm @global do gem update --system
+# something is broken with gem wrappers
+# /usr/local/rvm/bin/rvm @global do gem update --system 3.2.2
 /usr/local/rvm/bin/rvm @global do gem update bundler --force
-/usr/local/rvm/bin/rvm @global do gem install gem-wrappers
 /usr/local/rvm/bin/rvm alias create default 2.6.6@webapp --create
 gem install pry
