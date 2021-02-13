dbb81db7256f11cac75bd231a29aefa06421d185
Jason Young <jay@outfielding.net>
2021-02-13 10:12:07 -0500

    sorting fixes

--- app/models/draft_batter.rb
+++ app/models/draft_batter.rb
@@ -32,14 +32,14 @@ class DraftBatter < DraftPlayer
   }
 
   scope :byrankingvalue, lambda {|rv|
-    select("#{self.table_name}.*, draft_rankings.value as rankvalue").joins(:draft_rankings).where("draft_rankings.draft_ranking_value_id = #{rv.id}").order("rankvalue DESC")
+    select("#{self.table_name}.*, draft_rankings.value as rankvalue").joins(:draft_rankings).where("draft_rankings.draft_ranking_value_id = #{rv.id}").order("rankvalue DESC,#{self.table_name}.id ASC").order("#{self.table_name}.id ASC")
   }
 
   scope :byrankingvalue_and_wantedowner, lambda {|rv,owner|
     select("#{self.table_name}.*, draft_rankings.value as rankvalue,draft_wanteds.notes as notes,draft_wanteds.highlight as highlight")
     .joins([:draft_rankings,:wantedowners])
     .where("draft_rankings.draft_ranking_value_id = #{rv.id} and owners.id = #{owner.id}")
-    .order("rankvalue DESC")
+    .order("rankvalue DESC").order("#{self.table_name}.id ASC")
   }
 
 end
