57ccde5544607f2335984ef51924fd8c16162545
Jason Young <jay@outfielding.net>
2021-03-28 12:04:30 -0400

    remove logging

--- app/assets/javascripts/channels/notify_draft_pick.js
+++ app/assets/javascripts/channels/notify_draft_pick.js
@@ -7,11 +7,9 @@
     received: function(data) {
       var currentPickedCount = data.picked_count;
       var browserPickedCount= Cookies.get('picked_count');
-      console.log('got data!')
 
       if(browserPickedCount) {
         if(currentPickedCount != browserPickedCount) {
-          console.log('counts not equal')
           Cookies.set('picked_count', currentPickedCount);
           window.location.replace(window.location.href);
         }
