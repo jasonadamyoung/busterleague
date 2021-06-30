ebe0dc8a0aa7465eb9da14f42b038fbe89e6c5de
Jason Young <jay@outfielding.net>
2021-06-30 19:40:33 -0400

    fix issues

--- build/license_compliance.sh
+++ build/license_compliance.sh
@@ -1,3 +1,9 @@
 # additional packages for license_compliance
 apt-get update
 apt-get install -y sudo tzdata libmagickwand-dev imagemagick dnsutils iputils-ping postgresql-client
+apt-get install -y --no-install-recommends build-essential zlib1g-dev libssl-dev
+# weird
+bundle config --local path vendor
+bundle config --local jobs "$(nproc)"
+bundle install
+yarn install
